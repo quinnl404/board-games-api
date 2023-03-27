9ae57720be36f3c0c321bbde9d94edc79ca8b175
Quinn Lord <quinndolin404@gmail.com>
2023-03-27 16:14:28 +0100

    refactor testing every review has data of the right type to checking whether a single review is exactly correct

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -56,25 +56,23 @@ describe("GET /api/reviews/:review_id", () => {
   });
 
   it("200: returns a review object with correct properties", () => {
-    const path = "/api/reviews/";
-    for (let index = 1; index < 14; index++) {
-      return request(app)
-        .get(path + String(index))
-        .expect(200)
-        .then(({ body }) => {
-          const { review } = body;
-          expect(review).toMatchObject({
-            review_id: expect.any(Number),
-            title: expect.any(String),
-            review_body: expect.any(String),
-            designer: expect.any(String),
-            owner: expect.any(String),
-            review_img_url: expect.any(String),
-            votes: expect.any(Number),
-            category: expect.any(String),
-            created_at: expect.any(String),
-          });
+    return request(app)
+      .get("/api/reviews/4")
+      .expect(200)
+      .then(({ body }) => {
+        const { review } = body;
+        expect(review).toMatchObject({
+          title: "Dolor reprehenderit",
+          designer: "Gamey McGameface",
+          owner: "mallionaire",
+          review_img_url:
+            "https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?w=700&h=700",
+          review_body:
+            "Consequat velit occaecat voluptate do. Dolor pariatur fugiat sint et proident ex do consequat est. Nisi minim laboris mollit cupidatat et adipisicing laborum do. Sint sit tempor officia pariatur duis ullamco labore ipsum nisi voluptate nulla eu veniam. Et do ad id dolore id cillum non non culpa. Cillum mollit dolor dolore excepteur aliquip. Cillum aliquip quis aute enim anim ex laborum officia. Aliqua magna elit reprehenderit Lorem elit non laboris irure qui aliquip ad proident. Qui enim mollit Lorem labore eiusmod",
+          category: "social deduction",
+          created_at: "2021-01-22T11:35:50.936Z",
+          votes: 7,
         });
-    }
+      });
   });
 });
